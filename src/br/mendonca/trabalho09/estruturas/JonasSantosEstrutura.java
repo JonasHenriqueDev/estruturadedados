23bf39738b5d09eb1e7ea73c8ef11d7fcf9f5792
jonas <jonas.henrique@upe.br>
2023-08-08 20:12:40 -0300

    commit2

--- src/br/mendonca/trabalho09/estruturas/JonasSantosEstrutura.java
+++ src/br/mendonca/trabalho09/estruturas/JonasSantosEstrutura.java
@@ -4,6 +4,9 @@ import br.mendonca.trabalho09.ITAD;
 import br.mendonca.trabalho09.ITrabalho09;
 import br.mendonca.trabalho09.elementos.JonasSantosElemento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JonasSantosEstrutura implements ITrabalho09 {
 
     private JonasSantosElemento cursor;
@@ -15,12 +18,24 @@ public class JonasSantosEstrutura implements ITrabalho09 {
 
     @Override
     public int getQuantidade() {
-        return 0;
+        return quantidade;
     }
 
     @Override
     public Object getLista() {
-        return null;
+        if (cursor == null) {
+            return null;
+        }
+
+        List<ITAD> lista = new ArrayList<>();
+        JonasSantosElemento current = cursor;
+
+        do {
+            lista.add(current.getTad());
+            current = (JonasSantosElemento) current.getNext();
+        } while (current != cursor);
+
+        return lista;
     }
 
     @Override
@@ -44,36 +59,97 @@ public class JonasSantosEstrutura implements ITrabalho09 {
 
     @Override
     public void inserirDepois(ITAD tad) {
+        JonasSantosElemento novo = new JonasSantosElemento();
+        novo.setTad (tad);
 
+        if (cursor == null) {
+            cursor = novo;
+            cursor.setNext(novo);
+            cursor.setPrevious(novo);
+        } else {
+            novo.setNext(cursor.getNext());
+            novo.setPrevious(cursor);
+            JonasSantosElemento proximo = (JonasSantosElemento) cursor.getNext();
+            proximo.setPrevious(novo);
+            cursor.setNext(novo);
+        }
+
+        quantidade++;
     }
 
     @Override
     public void deslocarCursor(int posicao) {
-
+        while (posicao !=0) {
+            if (posicao > 0) {
+                cursor = (JonasSantosElemento) cursor.getNext();
+                posicao--;
+            } else {
+                cursor = (JonasSantosElemento) cursor.getPrevious();
+                posicao++;
+            }
+        }
     }
 
     @Override
     public ITAD removerAntes() {
-        return null;
+        if (quantidade == 0 || cursor == null) return null;
+
+        JonasSantosElemento anterior = (JonasSantosElemento) cursor.getPrevious();
+        ITAD tadRemovido = anterior.getTad();
+
+        if (quantidade == 1) {
+            cursor = null;
+        } else {
+            cursor.setPrevious(anterior.getPrevious());
+            anterior.getPrevious().setNext(cursor);
+        }
+
+        quantidade--;
+        return tadRemovido;
     }
 
     @Override
     public ITAD removerDepois() {
-        return null;
+        if (quantidade == 0 || cursor == null) return null;
+
+        JonasSantosElemento proximo = (JonasSantosElemento) cursor.getNext();
+        ITAD tadRemovido = proximo.getTad();
+
+        if (quantidade == 1) {
+            cursor = null;
+        } else {
+            cursor.setNext(proximo.getNext());
+            proximo.getNext().setPrevious(cursor);
+        }
+
+        quantidade--;
+        return tadRemovido;
     }
 
     @Override
     public ITAD getAtual() {
-        return null;
+        if (cursor == null) return null;
+
+        return cursor.getTad();
     }
 
     @Override
     public ITAD get(int posicao) {
-        return null;
+        if (cursor == null) return null;
+
+        return cursor.getTad();
     }
 
     @Override
     public String print() {
-        return null;
+        String msg = "\nLISTA SEQUENCIAL DINAMICA CIRCULAR DUPLAMENTE ENCADEADA\n";
+        if (cursor != null) {
+            JonasSantosElemento inicio = cursor;
+            do {
+                msg += inicio.getTad().print();
+                inicio = (JonasSantosElemento) inicio.getNext();
+            } while (inicio != cursor);
+        }
+        return msg;
     }
 }
